156e46b1e2096d7e46c2d6dc5c519e5d106b4dac
mguler47 <mehmetgulerdevops@gmail.com>
2020-04-08 15:37:25 -0500

    edit

--- artemis_delivery.groovy
+++ artemis_delivery.groovy
@@ -25,39 +25,4 @@ properties(
 				}
 			}
 		}
-		stage("Build Docker Image"){
-			timestamps {
-				ws {
-					sh '''
-						docker build -t artemis:${Version} .
-						'''
-				}
-			}
-		}
-		stage("Tag Image"){
-			timestamps {
-				ws {
-					sh '''
-						docker tag artemis:${Version} 713287746880.dkr.ecr.us-east-1.amazonaws.com/artemis:${Version}
-						'''
-				}
-			}
-		}
-		stage("Push Image"){
-			timestamps {
-				ws {
-					sh '''
-						docker push 713287746880.dkr.ecr.us-east-1.amazonaws.com/artemis:${Version}
-						'''
-				}
-			}
-		}
-		stage("Send slack notifications"){
-			timestamps {
-				ws {
-					echo "Slack"
-					//slackSend color: '#BADA55', message: 'Hello, World!'
-				}
-			}
-		}
-	}
+    }
