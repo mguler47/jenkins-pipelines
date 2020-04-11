5b7ae29ddb7f5633a29a99842c100b4e2a0dc702
mguler47 <mehmetgulerdevops@gmail.com>
2020-04-11 18:38:50 -0500

    edit last

--- artemis_delivery2.groovy
+++ artemis_delivery2.groovy
@@ -69,8 +69,7 @@ node {
 			timestamps {
 				ws {
 					sh '''
-                        aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 549828394506.dkr.ecr.us-east-1.amazonaws.com/artemis
-						ssh centos@dev1.senamina.com $(aws ecr get-login --no-include-email --region us-east-1)
+                       ssh centos@dev1.senamina.com $(aws ecr get-login --no-include-email --region us-east-1)
 						'''
 				}
 			}
